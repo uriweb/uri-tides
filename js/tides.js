ac15f8950b957df0e7f7b12251039ea1cf49da6f
Brandon Fuller <bjcfuller@uri.edu>
2017-09-11 14:25:41 -0400

    Plot a vertical indication of water height; ideally this would be positioned horizontally as well to track the static SVG curve

--- js/tides.js
+++ js/tides.js
@@ -6,11 +6,11 @@
      * See API documentation at https://tidesandcurrents.noaa.gov/api/
      */
     var parameters = {
-        product: 'water_level',
+        product: 'water_level', // use 'predictions' for predicted data
         date: 'recent',
         datum: 'MLLW',
         station: '8454049',
-        time_zone: 'LST',
+        time_zone: 'GMT',
         units: 'english'
     };  
     
@@ -48,7 +48,7 @@
 			}
 		};
         
-        url = 'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&date=today&datum=MLLW&station=8454049&time_zone=lst_ldt&units=english&interval=hilo&format=json';
+        url = 'https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&date=today&datum=MLLW&station=8454049&time_zone=gmt&units=english&interval=hilo&format=json';
 		xmlhttp.open('GET', url, true);
 		xmlhttp.send();
     
@@ -87,8 +87,9 @@
      * @param raw json the returned data
      */
     function buildChart(el, tides, data) {
-		data = data.data,        
-        console.log(data);
+		data = data.data; // Use data.predictions when fetching prediction data
+        
+        //console.log(data);
         
         var current, previous, tide, output, n = data.length;
                 
@@ -115,19 +116,17 @@
         
         
         // Build the Graphic SVG
-        console.log(tides.predictions[0]);
         
-        var predictions = tides.predictions;
-        for (var x in predictions) {
-            console.log(predictions[x]);
-        }
-        
-        output += '<svg height="30px" width="100px" class="tidechart">';
+        var predictions = tides.predictions,
+            diff = Math.abs(predictions[0].v - predictions[1].v),
+            h = 30; // the SVG height
+                
+        output += '<svg height="' + h + 'px" width="100px" class="tidechart">';
         output += '<g transform="matrix(0.830841,0,0,0.872634,-11809.4,-4442.24)">';
         output += '<path d="M14215,5107.81C14215,5107.81 14226.3,5091.67 14245,5091.8C14263.7,5091.94 14284.2,5123.99 14305,5123.81C14325.7,5123.62 14333,5107.81 14333,5107.81" style="fill:none;stroke:black;stroke-width:2.36px;"/>';
         output += '</g>'
         
-        
+        output += '<circle cx="50" cy="' + (h - h / diff * current) + '" r="5" stroke="black" stroke-width="0" fill="#000"" />';
         
         output += '</svg>';
         
