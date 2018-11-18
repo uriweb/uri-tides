df1ef764228e3b07ca8bcfc40a7d978a2cdd8227
John Pennypacker <jpennypacker@uri.edu>
2018-11-18 15:23:13 -0500

    fixed a bug codacy found.

--- uri-tides.php
+++ uri-tides.php
@@ -178,12 +178,12 @@ function uri_tides_get_data() {
 function _uri_tides_notify_administrator( $tides_data ) {
 	// @todo: identify which site is sending the error
 	$to = get_option('admin_email');
-	if( empty ( $admin_email ) ) {
+	if( empty ( $to ) ) {
 		$to = 'jpennypacker@uri.edu';
 	}
-	$tz = get_option('timezone_string');
-	$date = (new DateTime('@' . $tides_data['date']))->setTimezone(new DateTimeZone( $tz ));
-	$expiry = (new DateTime('@' . $tides_data['expires_on']))->setTimezone(new DateTimeZone( $tz ));
+	$timezone = get_option('timezone_string');
+	$date = (new DateTime('@' . $tides_data['date']))->setTimezone(new DateTimeZone( $timezone ));
+	$expiry = (new DateTime('@' . $tides_data['expires_on']))->setTimezone(new DateTimeZone( $timezone ));
 
 	$subject = 'URI Tides failed to update tide data';
 	$message = "The last time that tides data was refreshed successfully was on: " . $date->format( 'Y-m-d\TH:i:s' );
